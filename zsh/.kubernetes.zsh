225985e2280c8eb7c78a1cfe673db7e608efa054
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-01-26 15:40:43 -0500

    fix: add additional aliases

--- zsh/.kubernetes.zsh
+++ zsh/.kubernetes.zsh
@@ -1,10 +1,15 @@
 alias k='kubecolor' 
 alias kgp='kubecolor get pods'
 alias kgpw='kubecolor get pods -o wide'
+alias kgpww='kubecolor get pods -o wide --watch'
 alias kgnw='kubecolor get nodes -o wide'
 alias ke='kubecolor get events'
 alias kn='kubecolor config set-context --current --namespace'
 alias ka='kubecolor apply -f'
+alias kd='kubecolor delete -f'
+alias ke='kubecolor events'
+alias kew='kubecolor events --watch'
+
 
 
 compdef kubecolor=kubectl
