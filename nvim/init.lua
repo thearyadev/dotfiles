e0c9f9bb02d108f8415d78fd1ce68abcab9efb80
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-01-26 15:34:17 -0500

    fix: remove duplicate lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -182,7 +182,7 @@ local servers = {}
 require("neodev").setup()
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+-- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local capabilities2 = require("blink.cmp").get_lsp_capabilities()
 
@@ -208,55 +208,55 @@ mason_lspconfig.setup_handlers({
 require("lspconfig").sourcekit.setup({})
 
 
-local cmp = require("cmp")
-local luasnip = require("luasnip")
-require("luasnip.loaders.from_vscode").lazy_load()
-luasnip.config.setup({})
+-- local cmp = require("cmp")
+-- local luasnip = require("luasnip")
+-- require("luasnip.loaders.from_vscode").lazy_load()
+-- luasnip.config.setup({})
 
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	completion = {
-		completeopt = "menu,menuone,noinsert",
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete({}),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.locally_jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "path" },
-	},
-})
+-- cmp.setup({
+-- 	snippet = {
+-- 		expand = function(args)
+-- 			luasnip.lsp_expand(args.body)
+-- 		end,
+-- 	},
+-- 	completion = {
+-- 		completeopt = "menu,menuone,noinsert",
+-- 	},
+-- 	mapping = cmp.mapping.preset.insert({
+-- 		["<C-n>"] = cmp.mapping.select_next_item(),
+-- 		["<C-p>"] = cmp.mapping.select_prev_item(),
+-- 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+-- 		["<C-f>"] = cmp.mapping.scroll_docs(4),
+-- 		["<C-Space>"] = cmp.mapping.complete({}),
+-- 		["<CR>"] = cmp.mapping.confirm({
+-- 			behavior = cmp.ConfirmBehavior.Replace,
+-- 			select = true,
+-- 		}),
+-- 		["<Tab>"] = cmp.mapping(function(fallback)
+-- 			if cmp.visible() then
+-- 				cmp.select_next_item()
+-- 			elseif luasnip.expand_or_locally_jumpable() then
+-- 				luasnip.expand_or_jump()
+-- 			else
+-- 				fallback()
+-- 			end
+-- 		end, { "i", "s" }),
+-- 		["<S-Tab>"] = cmp.mapping(function(fallback)
+-- 			if cmp.visible() then
+-- 				cmp.select_prev_item()
+-- 			elseif luasnip.locally_jumpable(-1) then
+-- 				luasnip.jump(-1)
+-- 			else
+-- 				fallback()
+-- 			end
+-- 		end, { "i", "s" }),
+-- 	}),
+-- 	sources = {
+-- 		{ name = "nvim_lsp" },
+-- 		{ name = "luasnip" },
+-- 		{ name = "path" },
+-- 	},
+-- })
 -- Define the command to replace "it-tools" with an argument
 vim.api.nvim_create_user_command('Rep', function(opts)
     -- Get the argument passed to the command
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -6,7 +6,6 @@ require("lazy").setup({
 	"edluffy/hologram.nvim",
 	"tpope/vim-sleuth",
 	"sbdchd/neoformat",
-	"github/copilot.vim",
 	"xiyaowong/transparent.nvim",
 	{
 		"saghen/blink.cmp",
@@ -18,7 +17,7 @@ require("lazy").setup({
 			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
 			-- see the "default configuration" section below for full documentation on how to define
 			-- your own keymap.
-			keymap = { preset = "default" },
+			keymap = { preset = "enter" },
 
 			appearance = {
 				use_nvim_cmp_as_default = true,
@@ -122,16 +121,16 @@ require("lazy").setup({
 	{
 		"ojroques/nvim-osc52",
 	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-			"rafamadriz/friendly-snippets",
-		},
-	},
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	dependencies = {
+	-- 		"L3MON4D3/LuaSnip",
+	-- 		"saadparwaiz1/cmp_luasnip",
+	-- 		"hrsh7th/cmp-nvim-lsp",
+	-- 		"hrsh7th/cmp-path",
+	-- 		"rafamadriz/friendly-snippets",
+	-- 	},
+	-- },
 	{
 		"folke/which-key.nvim",
 		opts = {},
